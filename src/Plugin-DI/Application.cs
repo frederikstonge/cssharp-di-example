136b1ba1a55fc3208728da52ffd367806f3880a9
Frederik St-Onge <frederik.stonge@gmail.com>
2023-11-13 15:54:56 -0500

    Add example of event registrations

--- src/Plugin-DI/Application.cs
+++ src/Plugin-DI/Application.cs
@@ -1,3 +1,6 @@
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Timers;
 using Plugin.DI.Models;
 using Plugin.DI.Services;
 
@@ -15,5 +18,27 @@ public class Application : IApplication
         _plugin = plugin;
         _config = config;
         _pluginService = pluginService;
+
+        _plugin.AddCommand("test", "test method", Test);
+        _plugin.RegisterListener<Listeners.OnMapStart>(OnMapStart);
+        _plugin.RegisterEventHandler<EventPlayerDeath>(OnPlayerDeath);
+        _plugin.AddTimer(0.25f, OnTimerTick, TimerFlags.REPEAT | TimerFlags.STOP_ON_MAPCHANGE);
+    }
+
+    public void Test(CCSPlayerController? client, CommandInfo commandInfo)
+    {
+    }
+
+    public void OnMapStart(string mapName)
+    {
+    }
+
+    public HookResult OnPlayerDeath(EventPlayerDeath @event, GameEventInfo gameEventInfo)
+    {
+        return HookResult.Continue;
+    }
+
+    public void OnTimerTick()
+    {
     }
 }
